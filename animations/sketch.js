9fb1ee1ae6f09dac384e9be792e9dfc7c4df446d
LucasV26 <silva.sousa@aluno.ifsp.edu.br>
2020-07-27 21:01:26 -0300

    Adicionando interação entre os objetos e possibilitando remoção de objetos da tela

--- animations/sketch.js
+++ animations/sketch.js
@@ -1,43 +1,5 @@
 //Criando um Mouse e um ponto flutiante que anda em linha reta com ângulos aleatoriamente variados
 //Creating a Mouse and a floating point that moves in a strait line with random variated angles
-
-class Triangulo{
-  constructor(p1x, p1y, p2x, p2y, p3x, p3y){
-    this.p1x = p1x;
-    this.p1y = p1y;
-    this.p2x = p2x;
-    this.p2y = p2y;
-    this.p3x = p3x;
-    this.p3y = p3y;
-    this.a = dist(p1x, p1y, p2x, p2y);
-    this.b = dist(p2x, p2y, p3x, p3y);
-    this.c = dist(p3x, p3y, p1x, p1y);
-  }
-
-  Area(){
-    let p = (this.a+this.b+this.c)/2;
-    let area = Math.sqrt(p * (p-this.a) * (p-this.b) * (p-this.c));
-    return Math.round(area);
-  }
-}
-class mouseTriangles{
-  constructor(mouseP1, mouseP2, mouseP3){
-    this.mouseP1 = mouseP1;
-    this.mouseP2 = mouseP2;
-    this.mouseP3 = mouseP3;
-  }
-
-  Areas(a, b, c){
-    let p1 = (this.mouseP2+b+this.mouseP3)/2;
-    let area1 = Math.sqrt(p1 * (p1-this.mouseP2) * (p1-b) * (p1-this.mouseP3));
-    let p2 = (this.mouseP1+this.mouseP3+c)/2;
-    let area2 = Math.sqrt(p2 * (p2-this.mouseP1) * (p2-this.mouseP3) * (p2-c));
-    let p3 = (a+this.mouseP1+this.mouseP2)/2;
-    let area3 = Math.sqrt(p3 * (p3-a) * (p3-this.mouseP1) * (p3-this.mouseP2));
-    return Math.round(area1+area2+area3);
-  }
-}
-
 function Aleatorio(maximo, minimo=0){
   let numero = Math.floor(Math.random() * maximo + minimo);
   return numero;
@@ -46,11 +8,10 @@ function Aleatorio(maximo, minimo=0){
 //Function to return a random number with a min |--| max range
 
 class Bolinha{
-  constructor(posX, posY, tamW, tamH, dir){
+  constructor(posX, posY, tamD, dir){
     this.X = posX;
     this.Y = posY;
-    this.W = tamW;
-    this.H = tamH;
+    this.D = tamD;
     this.dir = dir;
     this.incX = Aleatorio(2)==0?-2:2;
     this.incY = 1;
@@ -59,26 +20,61 @@ class Bolinha{
     this.R = Aleatorio(255);
     this.G = Aleatorio(255);
     this.B = Aleatorio(255);
+    this.A = 255;
+  }
+
+  trocandoCor(){
+    this.R = random(255);
+    this.R = random(255);
+    this.B = random(255);
+  }
+
+  static Rebatendo(bola1, bola2){
+    if(dist(bola1.X, bola1.Y, bola2.X, bola2.Y) <= bola1.D/2 + bola2.D/2){
+      bola1.trocandoCor();
+      bola2.trocandoCor();
+      if(bola1.X > bola2.X){
+        bola1.incX = 2;
+        bola2.incX = -2;
+      }else{
+        bola1.incX = -2;
+        bola2.incX = 2;
+      }
+      if(bola1.Y > bola2.Y){
+        bola1.dir = 'd';
+        bola2.dir = 'u';
+      }else{
+        bola1.dir = 'u';
+        bola2.dir = 'd';
+      }
+    }
+  }
+
+  trocandoOpacidade(a){
+    this.A = a;
+  }
+
+  pontoDentro(px, py){
+    let d = dist(this.X, this.Y, px, py);
+    return (d <= this.D/2);
   }
 
   batendoBordas(){
-    if(this.X+(this.W/2) >= width || this.X-(this.W/2) <= 0 || this.Y+(this.H/2) >= height || this.Y-(this.H/2) <= 0){
-      this.p2Y = Aleatorio(200, 50);
-      this.p2X = Aleatorio(300, 100);
-      this.R = Aleatorio(255);
-      this.R = Aleatorio(255);
-      this.B = Aleatorio(255);
+    if(this.X+(this.D/2) >= width || this.X-(this.D/2) <= 0 || this.Y+(this.D/2) >= height || this.Y-(this.D/2) <= 0){
+      this.p2Y = random(50, 200);
+      this.p2X = random(100, 300);
+      this.trocandoCor();
     }
-    if(this.X+(this.W/2) >= width){
+    if(this.X+(this.D/2) >= width){
       this.incX = -2;
     }
-    if(this.X-(this.W/2) <= 0){
+    if(this.X-(this.D/2) <= 0){
       this.incX = 2;
     }
-    if(this.Y+(this.H/2) >= height){
+    if(this.Y+(this.D/2) >= height){
       this.dir = 'u';
     }
-    if(this.Y-(this.H/2) <= 0){
+    if(this.Y-(this.D/2) <= 0){
       this.dir = 'd';
     }
   }
@@ -100,18 +96,26 @@ class Bolinha{
     this.X += this.incX;
     this.Y = this.incY;
   }
+
+  desenharBolinha(){
+    fill(this.R, this.G, this.B, this.A);
+    stroke(this.B, this.R, this.G);
+    ellipse(this.X, this.Y, this.D);
+  }
 }; 
 //Classe construtora do objeto 'Bolinha' que possui todas as variáveis e métodos para criar e mover a ellipse pela tela 
 //Constructor class of the object 'Bolinha' that posses all the variables and methods to create and move an ellipse across the screen
 
 let todasBolinhas = [
-  new Bolinha(100, 200, 50, 50, 'd')
+  new Bolinha(100, 200, 50, 'd')
 ]; 
 //O vetor 'todasBolinhas' armazena todos os objetos 'Bolinha' criados, permitindo a criação de N objetos funcionais
 //The array 'todasBolinhas' stores all the objects 'Bolinha' created, allowing the creation of N functional objects
-
+let TD;
+let mouseTD;
 function setup() { 
   createCanvas(700, 700);
+  TD = new Triangulo(0.95*width, 0.9*height, 0.85*width, 0.85*height, 0.85*width, 0.95*height);
 }; 
 //A função setup() é executada apenas 1 vez durante todo o fluxo
 //The setup() function runs only once during the whole program flow
@@ -126,16 +130,20 @@ function draw() {
   line((mouseX+10 + mouseX+30)/2, mouseY+40, ((mouseX+15 + mouseX+30)/2) + 5, mouseY+50); //Criando barrinha do ponteiro
   ///////////////////////////////////////////////////////////////////////////////////////////
   //Ponto:
-  for(let i in todasBolinhas){
-    fill(todasBolinhas[i].R, todasBolinhas[i].G, todasBolinhas[i].B);
-    stroke(todasBolinhas[i].B, todasBolinhas[i].R, todasBolinhas[i].G);
-    ellipse(todasBolinhas[i].X, todasBolinhas[i].Y, todasBolinhas[i].W, todasBolinhas[i].H);
+  for(let i = 0; i < todasBolinhas.length; i++){
+    todasBolinhas[i].desenharBolinha();
+    if(todasBolinhas[i].pontoDentro(mouseX, mouseY)){
+      todasBolinhas[i].trocandoOpacidade(100);
+    }else{
+      todasBolinhas[i].trocandoOpacidade(255);
+    }
     todasBolinhas[i].movendoBolinha();
+    for(let j = i + 1; j < todasBolinhas.length; j++){
+      Bolinha.Rebatendo(todasBolinhas[i], todasBolinhas[j]);
+    }
   }
-
-  let TD = new Triangulo(0.95*width, 0.9*height, 0.85*width, 0.85*height, 0.85*width, 0.95*height);
-  let mouseTD = new mouseTriangles(dist(mouseX, mouseY, TD.p1x, TD.p1y), dist(mouseX, mouseY, TD.p2x, TD.p2y), dist(mouseX, mouseY, TD.p3x, TD.p3y));
   
+  mouseTD = new mouseTriangles(dist(mouseX, mouseY, TD.p1x, TD.p1y), dist(mouseX, mouseY, TD.p2x, TD.p2y), dist(mouseX, mouseY, TD.p3x, TD.p3y));
   fill(0);
   if(mouseTD.Areas(TD.a, TD.b, TD.c) == TD.Area()){
     fill(0,255,0);
@@ -146,14 +154,19 @@ function draw() {
 //The draw() function runs infinitely
 
 function mousePressed(){
-  let TD = new Triangulo(0.95*width, 0.9*height, 0.85*width, 0.85*height, 0.85*width, 0.95*height);
-  let mouseTD = new mouseTriangles(dist(mouseX, mouseY, TD.p1x, TD.p1y), dist(mouseX, mouseY, TD.p2x, TD.p2y), dist(mouseX, mouseY, TD.p3x, TD.p3y));
-
+  let aux = 1;
   if(mouseTD.Areas(TD.a, TD.b, TD.c) == TD.Area()){
     location.replace('https://lucasv26.github.io/Estudando-p5js/animations/index_graphics.html');
   }
-
-  todasBolinhas.push(new Bolinha(mouseX, mouseY, Aleatorio(255), Aleatorio(255), Aleatorio(2)==0?'u':'d'))
+  for(let i = todasBolinhas.length-1; i >= 0; i--){
+    if(todasBolinhas[i].pontoDentro(mouseX, mouseY)){
+      todasBolinhas.splice(i,1);
+      aux = 0;
+    }
+  }
+  if(aux){
+    todasBolinhas.push(new Bolinha(mouseX, mouseY, random(50, 70), random(2)==0?'u':'d'));
+  }
 }; 
 //A função mousePressed() só é chamada quando o botão do mouse é pressionado (Ela pode nunca ocorrer)
 //Funções deste gênero são chamadas de Eventos.
