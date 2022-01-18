973fda720672abc12862de29dfd8be87bcff65f6
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2022-01-18 19:26:58 -0300

    Criando página de menu para fácil acesso aos sketches de P5 adicionados ao repositório

---
+++ index.js
@@ -0,0 +1,60 @@
+const pagesData = [
+    {
+        name: "Movimento em linha reta",
+        description: "Sketch sobre a fórmula de movimento retilínio. Cada bola segue sua própria função de reta",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index.html"
+    },
+    {
+        name: "Reflexão com desenho permanente",
+        description: "Sketch sobre a reflexão de um corpo nas extremidades na tela. O rastro da bola é fixado na tela",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index_teste.html"
+    },
+    {
+        name: "Atração gravitacional isolada",
+        description: "Sketch sobre a atração gravitacional em sistema isolado (sem forças externas). É possível cirar novos corpos utilizando o mouse",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index_gravity.html"
+    },
+    {
+        name: "Atração gravitacional acelerada",
+        description: "Sketch sobre atração gravitacional com aceleração constante. É possível criar novos corpos, os antigos serão deletados",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index_gravidade.html"
+    },
+    {
+        name: "Planos distintos",
+        description: "Estudo sobre ferramenta de múltiplos planos de desenho. O quadrado vermelho nunca ficará afrente dos pontos amarelos",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index_graphics.html"
+    },
+    {
+        name: "Função de circunferência",
+        description: "Sketch sobre a função de uma circunferência, todas as linhas são geradas a partir da função baseada no centro e raio da circunferência",
+        picture: "",
+        path: "https://lucasv26.github.io/animations/index_circle.html"
+    }
+];
+
+function init() {
+    let pagesList = document.getElementById("pages");
+
+
+    for (p of pagesData) {
+        let pageInfo = document.createElement("div");
+        pageInfo.setAttribute('class', 'pageCard')
+
+        pageInfo.innerHTML = `
+            <h3>${p.name}</h3>
+            <p>${p.description}</p>
+            <br>
+            <a href="${p.path}">Acessar</a>
+        `;
+
+        pagesList.appendChild(pageInfo);
+    }
+}
+
+
+init();
