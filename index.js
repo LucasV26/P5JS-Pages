49c422cf115a8a12f83bd5e045c49b997788cede
LucasV26 <lucas2606.vinicius@gmail.com>
2022-03-23 17:50:24 -0300

    Padronizando nomes dos arquivos de animação

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ const pagesData = [
         name: "Reflexão com desenho permanente",
         description: "Sketch sobre a reflexão de um corpo nas extremidades na tela. O rastro da bola é fixado na tela",
         picture: "",
-        path: "https://lucasv26.github.io/P5JS-Pages/animations/ReflectMovement/index_teste.html"
+        path: "https://lucasv26.github.io/P5JS-Pages/animations/ReflectMovement/index.html"
     },
     {
         name: "Atração gravitacional isolada",
         description: "Sketch sobre a atração gravitacional em sistema isolado (sem forças externas). É possível cirar novos corpos utilizando o mouse",
         picture: "",
-        path: "https://lucasv26.github.io/P5JS-Pages/animations/IsolatedGravity/index_gravity.html"
+        path: "https://lucasv26.github.io/P5JS-Pages/animations/IsolatedGravity/index.html"
     },
     {
         name: "Atração gravitacional acelerada",
         description: "Sketch sobre atração gravitacional com aceleração constante. É possível criar novos corpos, os antigos serão deletados",
         picture: "",
-        path: "https://lucasv26.github.io/P5JS-Pages/animations/AceleratedGravity/index_gravidade.html"
+        path: "https://lucasv26.github.io/P5JS-Pages/animations/AceleratedGravity/index.html"
     },
     {
         name: "Planos distintos",
         description: "Estudo sobre ferramenta de múltiplos planos de desenho. O quadrado vermelho nunca ficará afrente dos pontos amarelos",
         picture: "",
-        path: "https://lucasv26.github.io/P5JS-Pages/animations/MultipleCanvas/index_graphics.html"
+        path: "https://lucasv26.github.io/P5JS-Pages/animations/MultipleCanvas/index.html"
     },
     {
         name: "Função de circunferência",
         description: "Sketch sobre a função de uma circunferência, todas as linhas são geradas a partir da função baseada no centro e raio da circunferência",
         picture: "",
-        path: "https://lucasv26.github.io/P5JS-Pages/animations/Circle/index_circle.html"
+        path: "https://lucasv26.github.io/P5JS-Pages/animations/Circle/index.html"
     }
 ];
 
